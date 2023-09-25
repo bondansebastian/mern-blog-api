522443859fb224a58dad4f6a74b219582b55d011
Bondan Sebastian <bondansebastian@gmail.com>
2023-09-25 22:06:27 +0700

    Attempt to reduce load time

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import './envConfig';
 import express from "express";
 import cors from "cors";
 import dbo from "./db/conn";
-import createAdmin from './middlewares/createAdmin';
+// import createAdmin from './middlewares/createAdmin';
 import indexRoutes from './routes/indexRoutes';
-import postRoutes from './routes/postRoutes';
-import authRoutes from './routes/authRoutes';
+// import postRoutes from './routes/postRoutes';
+// import authRoutes from './routes/authRoutes';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,10 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true,
 }));
-app.use(createAdmin());
-app.use(authRoutes);
+// app.use(createAdmin());
+// app.use(authRoutes);
 app.use(indexRoutes);
-app.use(postRoutes);
+// app.use(postRoutes);
 
 app.listen(port, () => {
     // Perform a database connection when server starts
